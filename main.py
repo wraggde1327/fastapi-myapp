727ee463e08aecf11e9d360a60e89e06bd891b26
Mahom <wraggde@mail.ru>
2025-06-23 16:55:27 +0800

    add root endpoint

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Главная страница
 @app.get("/")
 def read_root():
-    return {"message": "Привет с FastAPI!"}
-
-# Эндпоинт для приветствия по имени
-@app.get("/hello/{name}")
-def say_hello(name: str):
-    return {"message": f"Привет, {name}!"}
-
-# Эндпоинт для сложения двух чисел
-@app.get("/add")
-def add_numbers(a: int, b: int):
-    return {"result": a + b}
+    return {"message": "✅ FastAPI server is running!"}
